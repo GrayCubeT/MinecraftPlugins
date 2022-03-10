04efc0cd598e065788d168602fbcadae39572a50
GrayCubeT <Lazy.endermaks@gmail.com>
2022-03-10 15:09:19 +0300

    description added

---
+++ CharacterCheet/src/main/CharacterSheet.java
@@ -0,0 +1,17 @@
+package main;
+
+
+/*
+ * This class provides a basis for all data regarding any character
+ * contains all skills, buffs, values in easily-understandable form
+ * 
+ * It is attached to all players, npcs, custom mobs
+ */
+public class CharacterSheet {
+	
+	
+	
+	CharacterSheet() {
+		
+	}
+}	
