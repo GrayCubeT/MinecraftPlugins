576460e60bb3de25eb4684d027f1f813acd3babf
GrayCubeT <Lazy.endermaks@gmail.com>
2022-03-15 16:08:02 +0300

    javadoc added

--- CharacterCheet/src/main/CharacterSheet.java
+++ CharacterCheet/src/main/CharacterSheet.java
@@ -2,18 +2,24 @@ package main;
 
 import java.util.HashMap;
 
-/*
+/**
  * This class provides a basis for all data regarding any character
- * contains all skills, buffs, values in easily-understandable form
- * 
- * It is attached to all players, npcs, custom mobs
+ * contains all skills, buffs, values in easily-understandable form<br>
+ * <br>
+ * It is attached to all players, npcs, custom mobs<br>
+ * <br>
+ * must be saved to database every now and then in case server crashes<br>
+ * needs to be saved on player exits and fetched on player joins<br>
+ * <br>
+ * Since this is constantly used by server on almost every event, it must 
+ * be kept loaded in RAM
  */
 public class CharacterSheet {
 	int x = 6;
 	public CharacterSheet(int y) {
 		x = y;
 	}
-	
+	// this should be in the database though
 	public static void main(String[] args) {
 		CharacterSheet c = new CharacterSheet(5);
 		HashMap<String, Integer> map = new HashMap<String, Integer>();
